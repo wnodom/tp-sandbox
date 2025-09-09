5d37637071baa71dea2f779c639b9b4e8e36da8a
Bill Odom <bill@billodom.com>
2025-09-09 15:38:08 -0500

    Add basic layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { VideoList } from './video-list/video-list';
+import { VideoPlayer } from './video-player/video-player';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, VideoList],
+  imports: [RouterOutlet, VideoList, VideoPlayer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
