a62877769419b6cbc95260e794133eacb3d9ba6b
Bill Odom <bill@billodom.com>
2025-09-10 10:39:43 -0500

    Expose current video as a signal

--- src/app/video-data-handler.ts
+++ src/app/video-data-handler.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Video } from './video-types';
 import { BehaviorSubject } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,8 @@ export class VideoDataHandler {
   private _currentVideoSubject = new BehaviorSubject<Video | undefined>(undefined);
 
   readonly currentVideo$ = this._currentVideoSubject.asObservable();
+
+  readonly currentVideo = toSignal(this.currentVideo$);
 
   updateCurrentVideo(v: Video) {
     this._currentVideoSubject.next(v);
