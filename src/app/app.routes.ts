b3ffc085f54316eefa5296b63ecad0cbd1a94551
Bill Odom <bill@billodom.com>
2025-09-10 13:06:55 -0500

    Beef up routing a little

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { VideoDashboard } from './video-dashboard/video-dashboard';
 
-export const routes: Routes = [{ path: 'dashboard', component: VideoDashboard }];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: VideoDashboard },
+  { path: '**', redirectTo: 'dashboard' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('sandbox');
+  protected readonly title = signal('TP Sandbox');
 }
